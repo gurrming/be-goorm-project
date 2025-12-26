a2f0051ca11569011a849bc89f28245c8e9abb86
niceome <158061511+niceome@users.noreply.github.com>
2025-12-26 14:48:47 +0900

    first commit

---
+++ src/main/java/com/example/heartbit/HeartbitApplication.java
@@ -0,0 +1,13 @@
+package com.example.heartbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/heartbit/HeartbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heartbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
