ba21c862fbe1f231246e931f88a0b1dc0a7e6eaf
uujean12 <uujean12@gmail.com>
2025-12-30 14:51:03 +0900

    repo-trade-edit

--- src/main/java/com/example/heartbit/repository/TradeRepository.java
+++ src/main/java/com/example/heartbit/repository/TradeRepository.java
@@ -1,6 +1,8 @@
 package com.example.heartbit.repository;
 
 import com.example.heartbit.domain.Trade;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,18 +12,16 @@ import java.util.List;
 public interface TradeRepository extends JpaRepository<Trade, Long> {
 
     List<Trade> findByTradeBuyIdOrTradeSellId(Long buyOrderId, Long sellOrderId);
-    List<Trade> findByTradeTime();
+    Page<Trade> findAllByOrderByTradeTimeDesc(Pageable pageable);
 
     @Query("""
         SELECT t
         FROM Trade t
         JOIN t.buyOrder bo
         JOIN t.sellOrder so
-        WHERE bo.member = :memberId
-           OR so.member = :memberId
-        ORDER BY t.tradeTime DESC
+        WHERE bo.member.memberId = :memberId
+           OR so.member.memberId = :memberId
     """)
-    List<Trade> findTradeByMemberId(@Param("member") Long memberId);
-
+    Page<Trade> findTradeByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 
 }
