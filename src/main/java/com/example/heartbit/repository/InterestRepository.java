3e9b47b8277f30b28216626834e905f23032e6ab
uujean12 <uujean12@gmail.com>
2025-12-30 13:41:54 +0900

    inter-trade repository complate

--- src/main/java/com/example/heartbit/repository/InterestRepository.java
+++ src/main/java/com/example/heartbit/repository/InterestRepository.java
@@ -1,4 +1,14 @@
 package com.example.heartbit.repository;
 
-public interface InterestRepository {
+
+import com.example.heartbit.domain.Interest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InterestRepository extends JpaRepository<Interest, Long> {
+
+    List<Interest> findByMembertId(Long membertId);
+    boolean existsByMembertIdAndCategoryId(Long membertId, Long categoryId);
+    void deleteByMembertIdAndCategoryId(Long membertId, Long categoryId);
 }
--- src/main/java/com/example/heartbit/repository/TradeRepository.java
+++ src/main/java/com/example/heartbit/repository/TradeRepository.java
@@ -1,4 +1,27 @@
 package com.example.heartbit.repository;
 
-public interface TradeRepository {
+import com.example.heartbit.domain.Trade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface TradeRepository extends JpaRepository<Trade, Long> {
+
+    List<Trade> findByTradeBuyIdOrTradeSellId(Long buyOrderId, Long sellOrderId);
+    List<Trade> findByTradeTime();
+
+    @Query("""
+        SELECT t
+        FROM Trade t
+        JOIN t.buyOrder bo
+        JOIN t.sellOrder so
+        WHERE bo.member = :memberId
+           OR so.member = :memberId
+        ORDER BY t.tradeTime DESC
+    """)
+    List<Trade> findTradeByMemberId(@Param("member") Long memberId);
+
+
 }
