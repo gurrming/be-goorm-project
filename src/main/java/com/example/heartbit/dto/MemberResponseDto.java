8d696db70fcbf62c03e168d2af4c9dacbf6b097d
윤미나 <yunmina@yunminaui-MacBookPro.local>
2026-01-03 17:13:24 +0900

    feat: MemberResonseDTO 추가 로그인 성공시 accessToken, refrashToken, nickname 반환

--- src/main/java/com/example/heartbit/dto/MemberResponseDto.java
+++ src/main/java/com/example/heartbit/dto/MemberResponseDto.java
@@ -5,13 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
 public class MemberResponseDto {
-    private Long memberId;
-    private String memberEmail;
-    private String memberNickname;
-    // 비밀번호 안넣기
+
+    @Builder
+    public record MemberTokenDTO(
+            String accessToken,
+            String refreshToken
+    ){}
+
+    @Builder
+    public record MemberInfo(
+            String memberNickname
+    ){}
+
 }
