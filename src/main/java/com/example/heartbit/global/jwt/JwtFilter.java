39fe8ae2a0ce4406e1143e5220857a152d39a272
Yun Mina <dbsalsk2514@naver.com>
2026-01-04 19:50:22 +0900

    feat: 쿠키에서 accessToken 찾는 기능 추가
    로그인시 3가지 반환

--- src/main/java/com/example/heartbit/global/jwt/JwtFilter.java
+++ src/main/java/com/example/heartbit/global/jwt/JwtFilter.java
@@ -3,6 +3,7 @@ package com.example.heartbit.global.jwt;
 import com.example.heartbit.domain.Member;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -24,8 +25,15 @@ public class JwtFilter extends OncePerRequestFilter {
 
     private String resolveToken(HttpServletRequest request){
         String bearerToken = request.getHeader("Authorization");
-        if(bearerToken != null && bearerToken.startsWith("Bearer")){
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
             return bearerToken.substring(7);
+        }
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if ("accessToken".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
         }
         return null;
     }
