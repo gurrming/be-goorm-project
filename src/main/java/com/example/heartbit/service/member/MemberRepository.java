83a7548bb2b03e9bed81625cc98190c51f2bedad
Yun Mina <dbsalsk2514@naver.com>
2026-01-04 17:10:43 +0900

    delete: 불필요한 파일 삭제

--- src/main/java/com/example/heartbit/service/member/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.heartbit.service.member;
-
-import com.example.heartbit.domain.Member;
-import org.springframework.data.repository.Repository;
-
-interface MemberRepository extends Repository<Member, Long> {
-}
