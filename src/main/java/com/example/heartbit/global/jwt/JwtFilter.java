18f8f33a9d5e25ec9d77820c0092a94312ac45d7
Yun Mina <dbsalsk2514@naver.com>
2026-01-04 16:59:49 +0900

    feat: JwtFilter 추가

---
+++ src/main/java/com/example/heartbit/global/jwt/JwtFilter.java
@@ -0,0 +1,55 @@
+package com.example.heartbit.global.jwt;
+
+import com.example.heartbit.domain.Member;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private String resolveToken(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith("Bearer")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+        String token = resolveToken(request);
+
+        if(token != null && jwtTokenProvider.validateToken(token)){
+            String userId = jwtTokenProvider.getSubject(token);
+            List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_MEMBER"));
+
+            UserDetails principal = new User(userId,"", authorities);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    principal, null
+                    , Collections.singletonList(new SimpleGrantedAuthority("ROLE_MEMBER"))
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
