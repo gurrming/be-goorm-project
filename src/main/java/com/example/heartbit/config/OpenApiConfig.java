28ac17738d412ed37027cd9f8eb7f5381f3ebbb9
Yun Mina <dbsalsk2514@naver.com>
2026-01-05 13:21:05 +0900

    feat: OpenApiConfig 파일에 API 요청헤더에 인증정보 포함하는 코드, SecuritySchemes 등록 코드 추가

--- src/main/java/com/example/heartbit/config/OpenApiConfig.java
+++ src/main/java/com/example/heartbit/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.example.heartbit.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,15 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
+        String jwt = "JWT";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
         return new OpenAPI()
                 .info(new Info()
                         .title("heartbit API 명세서")
