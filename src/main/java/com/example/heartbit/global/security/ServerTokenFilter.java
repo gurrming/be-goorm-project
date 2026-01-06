10a581fd67a04897eb8f895c48cedd3898018efc
Yun Mina <dbsalsk2514@naver.com>
2026-01-06 13:17:25 +0900

    feat: ServerTokenFilter 추가 봇 토큰과 비교하여 같으면 jwtFilter 통과

---
+++ src/main/java/com/example/heartbit/global/security/ServerTokenFilter.java
@@ -0,0 +1,42 @@
+package com.example.heartbit.global.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ServerTokenFilter extends OncePerRequestFilter {
+
+    @Value("${server.secret-token}")
+    private String serverToken;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain
+    ) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader != null && authHeader.startsWith("Bearer")){
+            String token = authHeader.substring(7);
+            if(serverToken.equals(token)){
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        "BOT", null, List.of()
+                );
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+
+}
