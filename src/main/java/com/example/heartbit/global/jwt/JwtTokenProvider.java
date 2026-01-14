1c7913a2f2df40ac6d03fd659171d06edc5a3ada
Yun Mina <dbsalsk2514@naver.com>
2026-01-14 11:03:20 +0900

    feat: 토큰 발급 메서드 추가

--- src/main/java/com/example/heartbit/global/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/heartbit/global/jwt/JwtTokenProvider.java
@@ -1,15 +1,21 @@
 package com.example.heartbit.global.jwt;
 
+import com.example.heartbit.global.jwt.dto.IssuedTokens;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.internal.parser.TokenType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Date;
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -23,6 +29,8 @@ public class JwtTokenProvider {
 
     @Value("${jwt.refresh-token-expiration-time}")
     private long refreshTokenExpirationTime;
+
+    private final SecretKey signingKey;
 
 
     private Key getSignInKey(){
@@ -71,5 +79,43 @@ public class JwtTokenProvider {
             log.info("JWT 토큰이 잘못되었습니다.", e);
         }
         return false;
+    }
+
+    public String newJti(){
+        return UUID.randomUUID().toString();
+    }
+
+    public IssuedTokens issueNewTokens(long memberId, String deviceId) {
+        String jti = newJti();
+        Date now = new Date();
+
+        // 1. Access Token 생성
+        Date accessTokenExpiresIn = new Date(now.getTime() + accessTokenExpirationTime);
+        String accessToken = Jwts.builder()
+                .setSubject(String.valueOf(memberId))
+                .setExpiration(accessTokenExpiresIn)
+                .setIssuedAt(now)
+                .claim("typ", "ACCESS")
+                .claim("did", deviceId)
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+
+        // 2. Refresh Token 생성
+        Date refreshTokenExpiresIn = new Date(now.getTime() + refreshTokenExpirationTime);
+        String refreshToken = Jwts.builder()
+                .setSubject(String.valueOf(memberId))
+                .setExpiration(refreshTokenExpiresIn)
+                .setIssuedAt(now)
+                .claim("typ", "REFRESH")
+                .claim("did", deviceId)
+                .claim("jti", jti)
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+
+        // 3. 만료 시간(초 단위) 계산
+        long accessExpiresInSec = accessTokenExpirationTime / 1000;
+        long refreshExpiresInSec = refreshTokenExpirationTime / 1000;
+
+        return new IssuedTokens(accessToken, refreshToken, accessExpiresInSec, refreshExpiresInSec);
     }
 }
