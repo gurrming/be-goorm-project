a2a3a97193c869f16525de7139a5a8d79fa6b84a
Yun Mina <dbsalsk2514@naver.com>
2026-01-19 11:23:15 +0900

    feat: GlobalException, CustomerException 추가

--- src/main/java/com/example/heartbit/global/exception/GlobalException.java
+++ src/main/java/com/example/heartbit/global/exception/GlobalException.java
@@ -1,4 +1,25 @@
 package com.example.heartbit.global.exception;
 
+import com.example.heartbit.global.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
 public class GlobalException {
+    @ExceptionHandler(CustomerException.class)
+    public ResponseEntity<ApiResponse<Void>> handleCustomerException(CustomerException e){
+        ErrorCode errorCode = e.getErrorCode();
+
+        log.warn("Business Exception: {}", errorCode.getMessage());
+
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ApiResponse.onFailure(
+                        errorCode.getCode(),
+                        errorCode.getMessage()
+                ));
+    }
 }
