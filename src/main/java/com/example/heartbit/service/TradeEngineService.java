61b6bdd9137086d757f38b1cb8c011f7609e8cf1
niceome <158061511+niceome@users.noreply.github.com>
2026-01-21 17:15:39 +0900

    name change maching -> matching

--- src/main/java/com/example/heartbit/service/TradeEngineService.java
+++ src/main/java/com/example/heartbit/service/TradeEngineService.java
@@ -14,13 +14,13 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class TradeEngineService {
     // 주문 종목별 호가창
-    private final Map<Long, MatchingOrder> machingOrderBooks = new ConcurrentHashMap<>();
+    private final Map<Long, MatchingOrder> matchingOrderBooks = new ConcurrentHashMap<>();
     // 주문 값 dto로 반환
     public synchronized List<TradeResponse> processOrder(Order newOrder) {
         // 종목 id 값
         Long categoryId = newOrder.getCategory().getCategoryId();
 
-        MatchingOrder orderBook = machingOrderBooks.computeIfAbsent(categoryId, id -> new MatchingOrder());
+        MatchingOrder orderBook = matchingOrderBooks.computeIfAbsent(categoryId, id -> new MatchingOrder());
         // 새 주문
         orderBook.addOrderToBook(newOrder);
 
