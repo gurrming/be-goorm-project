38098aa054cb378020b3657bb808c14e47b27743
niceome <158061511+niceome@users.noreply.github.com>
2026-02-22 01:41:08 +0900

    healthController

--- src/main/java/com/example/heartbit/controller/HealthCheckController.java
+++ src/main/java/com/example/heartbit/controller/HealthCheckController.java
@@ -1,10 +1,12 @@
 package com.example.heartbit.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "헬스 체크 API", description = "서비스 상태 확인을 위한 헬스 체크 API")
 public class HealthCheckController {
 
     @GetMapping("/health")
