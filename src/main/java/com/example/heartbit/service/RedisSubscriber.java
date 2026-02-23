96e1f7eb135d8e917a09b2edb7a963f1e2672508
lbc <id4lbc@naver.com>
2026-02-23 16:56:08 +0900

    feat/notifyAndChatroomRedisSetting

--- src/main/java/com/example/heartbit/service/RedisSubscriber.java
+++ src/main/java/com/example/heartbit/service/RedisSubscriber.java
@@ -20,33 +20,25 @@ public class RedisSubscriber {
     private final ObjectMapper objectMapper;
     private final SimpMessagingTemplate messagingTemplate;
 
-//    /**
-//     * Redis에서 채팅 메시지가 Publish되면 호출됨
-//     */
-//    public void sendMessage(String publishMessage) {
-//        try {
-//            // Redis 발행 메시지를 ChatResponseDto로 변환
-//            ChatResponseDto chatResponse = objectMapper.readValue(publishMessage, ChatResponseDto.class);
-//            // 해당 카테고리 채팅방을 구독 중인 클라이언트들에게 전송
-//            messagingTemplate.convertAndSend("/topic/chat/" + chatResponse.getCategoryId(), chatResponse);
-//        } catch (Exception e) {
-//            log.error("채팅 구독 처리 중 오류 발생: {}", e.getMessage());
-//        }
-//    }
-//
-//    /**
-//     * Redis에서 알림 메시지가 Publish되면 호출됨
-//     */
-//    public void sendNotification(String publishMessage) {
-//        try {
-//            // Redis 발행 메시지를 NotificationResponseDto로 변환
-//            NotificationResponseDto notificationResponse = objectMapper.readValue(publishMessage, NotificationResponseDto.class);
-//            // 특정 사용자의 개인 알림 채널로 전송
-//            messagingTemplate.convertAndSend("/topic/notification/" + notificationResponse.getMemberId(), notificationResponse);
-//        } catch (Exception e) {
-//            log.error("알림 구독 처리 중 오류 발생: {}", e.getMessage());
-//        }
-//    }
+    public void sendChatUpdate(String publishMessage) {
+        try {
+            ChatResponseDto chatResponse = objectMapper.readValue(publishMessage, ChatResponseDto.class);
+
+            messagingTemplate.convertAndSend("/topic/chat/" + chatResponse.getCategoryId(), chatResponse);
+        } catch (Exception e) {
+            log.error("채팅 구독 메시지 처리 중 오류 발생: {}", e.getMessage());
+        }
+    }
+
+    public void sendNotificationUpdate(String publishMessage) {
+        try {
+            NotificationResponseDto notificationResponse = objectMapper.readValue(publishMessage, NotificationResponseDto.class);
+
+            messagingTemplate.convertAndSend("/topic/notification/" + notificationResponse.getMemberId(), notificationResponse);
+        } catch (Exception e) {
+            log.error("알림 구독 메시지 처리 중 오류 발생: {}", e.getMessage());
+        }
+    }
 
     public void sendTickerUpdate(String publishMessage) {
         try {
