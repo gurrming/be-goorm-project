93de4b447c23de05b87870292605970628119957
niceome <158061511+niceome@users.noreply.github.com>
2026-02-25 17:08:09 +0900

    dev

--- src/main/java/com/example/heartbit/config/Redis/RedisConfig.java
+++ src/main/java/com/example/heartbit/config/Redis/RedisConfig.java
@@ -1,7 +1,5 @@
 package com.example.heartbit.config.Redis;
 
-import com.example.heartbit.service.RedisOrderSubscriber;
-import com.example.heartbit.service.RedisSubscriber;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -13,9 +11,6 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -73,93 +68,5 @@ public class RedisConfig {
         return new StringRedisTemplate(connectionFactory);
     }
 
-    @Bean
-    public RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
-                                                   MessageListenerAdapter orderListenerAdapter,
-                                                   MessageListenerAdapter tickerListenerAdapter,
-                                                   MessageListenerAdapter tradesListenerAdapter,
-                                                   MessageListenerAdapter chartsListenerAdapter,
-                                                   MessageListenerAdapter orderbookPriceListenerAdapter,
-                                                   MessageListenerAdapter investListenerAdapter,
-                                                   MessageListenerAdapter orderbookListenerAdapter,
-                                                   MessageListenerAdapter chatListenerAdapter,
-                                                   MessageListenerAdapter notificationListenerAdapter) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(orderListenerAdapter, new ChannelTopic("order-sharding-channel"));
-        container.addMessageListener(tickerListenerAdapter, new ChannelTopic("ws-ticker-channel"));
-        container.addMessageListener(tradesListenerAdapter, new ChannelTopic("ws-trades-channel"));
-        container.addMessageListener(chartsListenerAdapter, new ChannelTopic("ws-charts-channel"));
-        container.addMessageListener(orderbookPriceListenerAdapter, new ChannelTopic("ws-orderbookPrice-channel"));
-        container.addMessageListener(orderbookListenerAdapter, new ChannelTopic("ws-orderbook-channel"));
-        container.addMessageListener(investListenerAdapter, new ChannelTopic("ws-invest-channel"));
-        container.addMessageListener(chatListenerAdapter, new ChannelTopic("ws-chat-channel"));
-        container.addMessageListener(notificationListenerAdapter, new ChannelTopic("ws-notification-channel"));
-        return container;
-    }
 
-    @Bean
-    public MessageListenerAdapter orderListenerAdapter(RedisOrderSubscriber subscriber) {
-        MessageListenerAdapter adapter = new MessageListenerAdapter(subscriber, "handleMessage");
-        adapter.setSerializer(RedisSerializer.json());
-        return adapter;
-    }
-
-    @Bean
-    public MessageListenerAdapter tickerListenerAdapter(RedisSubscriber subscriber) {
-        // RedisSubscriber 클래스의 "sendTickerUpdate" 메서드를 실행하라고 지정
-        MessageListenerAdapter adapter = new MessageListenerAdapter(subscriber, "sendTickerUpdate");
-        adapter.setSerializer(new StringRedisSerializer());
-        return adapter;
-    }
-
-    @Bean
-    public MessageListenerAdapter tradesListenerAdapter(RedisSubscriber subscriber) {
-        // RedisSubscriber 클래스의 "sendTradesUpdate" 메서드를 실행하라고 지정
-        MessageListenerAdapter adapter = new MessageListenerAdapter(subscriber, "sendTradesUpdate");
-        adapter.setSerializer(new StringRedisSerializer());
-        return adapter;
-    }
-
-    @Bean
-    public MessageListenerAdapter chartsListenerAdapter(RedisSubscriber subscriber) {
-        MessageListenerAdapter adapter = new MessageListenerAdapter(subscriber, "sendChartsUpdate");
-        adapter.setSerializer(new StringRedisSerializer());
-        return adapter;
-    }
-
-    @Bean
-    public MessageListenerAdapter orderbookPriceListenerAdapter(RedisSubscriber subscriber) {
-        MessageListenerAdapter adapter = new MessageListenerAdapter(subscriber, "sendOrderbookPriceUpdate");
-        adapter.setSerializer(new StringRedisSerializer());
-        return adapter;
-    }
-
-    @Bean
-    public MessageListenerAdapter investListenerAdapter(RedisSubscriber subscriber) {
-        MessageListenerAdapter adapter = new MessageListenerAdapter(subscriber, "sendInvestUpdate");
-        adapter.setSerializer(new StringRedisSerializer());
-        return adapter;
-    }
-
-    @Bean
-    public MessageListenerAdapter orderbookListenerAdapter(RedisSubscriber subscriber) {
-        MessageListenerAdapter adapter = new MessageListenerAdapter(subscriber, "sendOrderbookUpdate");
-        adapter.setSerializer(new StringRedisSerializer());
-        return adapter;
-    }
-
-    @Bean
-    public MessageListenerAdapter chatListenerAdapter(RedisSubscriber subscriber) {
-        MessageListenerAdapter adapter = new MessageListenerAdapter(subscriber, "sendChatUpdate");
-        adapter.setSerializer(new StringRedisSerializer());
-        return adapter;
-    }
-
-    @Bean
-    public MessageListenerAdapter notificationListenerAdapter(RedisSubscriber subscriber) {
-        MessageListenerAdapter adapter = new MessageListenerAdapter(subscriber, "sendNotificationUpdate");
-        adapter.setSerializer(new StringRedisSerializer());
-        return adapter;
-    }
 }
